5aa4475fc96cbde7fd9898b0b5f6cedf6d340053
Guillaume Ballet <gballet@gmail.com>
2019-11-28 16:26:39 +0100

    Wrote the precompile + test outline

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,56 @@
 extern crate multiproof_rs;
+extern crate rlp;
 
-use multiproof_rs::Multiproof;
+#[allow(non_upper_case_globals)]
+static mut input_data: &'static [u8] = &[0u8; 32];
+
+extern "C" {
+    static mut output: bool;
+    static input_size: usize;
+}
+
+use multiproof_rs::{rebuild, Multiproof};
+
+fn verify() -> Result<bool, String> {
+    // Get the data
+    unsafe {
+        if input_size < input_data.len() {
+            return Err(format!("input size exceeds allowed data"));
+        }
+    }
+    let input = unsafe { input_data.to_vec() };
+
+    // Deserialize the data into a tree
+    let proof = rlp::decode::<Multiproof>(&input).unwrap();
+    match rebuild(&proof) {
+        Ok(_) => Ok(true),
+        _ => Ok(false),
+    }
+}
 
 #[cfg(not(test))]
 #[no_mangle]
 pub extern "C" fn main() {
-    // Get the data
-
-    // Deserialize the data into a tree
-    let proof = rlp::decode::<Multiproof>();
-    let verified = match rebuild(&proof) {
-        Ok(_) => true,
-        None => false
+    let out = verify().unwrap();
+    unsafe {
+        output = out;
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::multiproof_rs::node::*;
+    use super::multiproof_rs::utils::*;
+    use super::multiproof_rs::*;
+    use super::*;
+
     #[test]
-    fn test_recover_account() {}
+    fn test_recover_account() {
+        let mut root = Node::default();
+        insert_leaf(&mut root, &NibbleKey::from(vec![0u8; 32]), vec![0u8; 32]).unwrap();
+        let encoding = rlp::encode(&root);
+        // TODO copy input data
+        unsafe { input_size = encoding.len() };
+        verify().unwrap();
+    }
 }
